4324ac93a2f1c065ba19ee28b2d736b43d1c66b9
amali <amali@LAPTOP>
2022-01-21 20:02:07 +0200

    Add project files.

---
+++ matrice_V4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int m, n, i, j, a[101][101], ok = 0, max = 0;
+	cin >> m >> n;
+	for (i = 1; i <= m; i++)
+		for (j = 1; j <= n; j++)
+			cin >> a[i][j];
+	for (i = 1; i <= m; i++)
+	{
+		for (j = 1; j <= n; j++)
+			if ((a[i][j] < 21) && (a[i][j] > max))
+				max = a[i][j];
+		for (j = 1; j <= n; j++)
+			if ((a[i][j] == max) && (ok == 0))
+			{
+				cout << a[i][j] << " ";
+				ok = 1;
+			}
+		if (ok == 0)
+			cout << "nu exista";
+		ok = 0;
+		max = 0;
+		cout << endl;
+	}
+	return 0;
+}
