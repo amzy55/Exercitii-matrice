cd4dcf2a9e991cfc9da8c2af4309ec83538fd1bc
Amália <amalia.zarcu@gmail.com>
2022-01-26 20:11:02 +0200

    V5

--- matrice_V5.cpp
+++ matrice_V5.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
+/*#include <iostream>
 
 using namespace std;
 
 int main()
 {
-	int m, n;
-
-}
+	int m, n, i, j, a[101][101];
+	cin >> m >> n;
+	for (i = 1; i <= m; i++)
+		for (j = 1; j <= n; j++)
+		{
+			a[i][j] = (i * j) % 10;
+		}
+	for (i = 1; i <= m; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}*/
