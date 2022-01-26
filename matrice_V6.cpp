7a8b8172630374b10131b4b85f02d9586efea733
Amália <amalia.zarcu@gmail.com>
2022-01-26 22:01:40 +0200

    V6

---
+++ matrice_V6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, i, j, a[21][21];
+	cin >> n;
+	for (i = 1; i <= n; i++)
+		for (j = 1; j <= n; j++)
+		{
+			if ((i + j) == n + 1)
+				a[i][j] = 0;
+			else if ((i + j) < n + 1)
+				a[i][j] = n - (i - 1) - j;
+			else
+				a[i][j] = a[i][j - 1] + 1;
+		}
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
