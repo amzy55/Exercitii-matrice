7f252c048739c313d6b80fbb0fdd995f52b6643f
Amália <amalia.zarcu@gmail.com>
2022-01-27 21:47:44 +0200

    V9

---
+++ matrice_V9.cpp
@@ -0,0 +1,22 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i, j, a[4][5] = { 0 };
+	for (i = 0; i < 4; i++)
+		for (j = 0; j < 5; j++)
+		{
+			a[i][j] = (i + j) % 3;
+		}
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}*/
