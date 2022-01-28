a5526045e19d429d20473b036998ba0b9d322f26
Amália <amalia.zarcu@gmail.com>
2022-01-28 03:12:23 +0200

    V11

--- matrice_V11.cpp
+++ matrice_V11.cpp
@@ -1,8 +1,33 @@
-#include <iostream>
+/*include <iostream>
 
 using namespace std;
 
 int main()
 {
+	int n, v[21][21], i, j, k;
+	cin >> n >> k;
 
-}
+	for (i = 1; i <= n; i++)
+		for (j = 1; j <= n; j++)
+		{
+			cin >> v[i][j];
+		}
+
+	for (i = 1; i <= k; i++)
+	{
+		j = v[k][i];
+		v[k][i] = v[i][k];
+		v[i][k] = j;
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			cout << v[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}*/
