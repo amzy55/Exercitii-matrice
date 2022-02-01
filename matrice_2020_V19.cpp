201194564ae0b8a7ea235a765d3592ae749ef6bb
Amália <amalia.zarcu@gmail.com>
2022-02-01 18:31:53 +0200

    V19

--- matrice_2020_V19.cpp
+++ matrice_2020_V19.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int m, n, k, v[20][20], i, j, ok, ok2;
+	int m, n, k, v[20][20], i, j, ok = 0, ok2 = 0;
 	cin >> m >> n >> k;
 
 	for (i = 1; i <= m; i++)
@@ -13,20 +13,22 @@ int main()
 			cin >> v[i][j];
 		}
 
-	for (i = 1; i <= m; i++)
+	for (i = 1; i <= m && ok2 == 0; i++)
 	{
-		ok = 1;
-		for (j = 1; j <= n; j++)
+		if (k != i)
 		{
-			if (k != i)
+			ok = 1;
+			for (j = 1; j <= n; j++)
+			{
 				if (v[k][j] == v[i][j])
 					ok = 0;
+			}
 		}
 		if (ok == 1)
 			ok2 = 1;
 	}
 
-	if (ok2 = 1)
+	if (ok2 == 1)
 	{
 		cout << "DA" << endl;
 		for (j = 1; j <= n; j++)
