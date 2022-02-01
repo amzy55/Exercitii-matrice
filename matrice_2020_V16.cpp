988c22fa52bd85beccabc0bb0acf004f3ef26f79
Amália <amalia.zarcu@gmail.com>
2022-02-01 17:45:11 +0200

    V16

---
+++ matrice_2020_V16.cpp
@@ -0,0 +1,26 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int v[100][100], i, j, m, n, k = 1;
+	cin >> m >> n;
+	for (i = m - 1; i >= 0; i--)
+		for (j = n - 1; j >= 0; j--)
+		{
+			v[i][j] = k * k;
+			k += 2;
+		}
+
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			cout << v[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}*/
